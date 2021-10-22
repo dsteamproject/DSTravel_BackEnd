2f97490fad88e69d94ccf14a7e9c4dc9cd843b3c
HoSeongYu <ghtjd026@gmail.com>
2021-10-22 18:12:58 +0900

    20211022
    20211022

---
+++ src/main/java/com/example/entity/Theme.java
@@ -0,0 +1,32 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@SequenceGenerator(initialValue = 301, name = "SEQ_THEME_NO", sequenceName = "SEQ_THEME_NO", allocationSize = 1)
+@Table(name = "THEME")
+public class Theme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_THEME_NO")
+    @Column(name = "NO")
+    private long tNo;
+    
+    @Column(name = "NAME")
+    private String tName;
+}
