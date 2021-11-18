ff224284c0c009f0f15f15f16262e6bab26599c6
HoSeongYu <ghtjd026@gmail.com>
2021-11-18 09:28:31 +0900

    hsyu travel update

--- src/main/java/com/example/controller/TravelController.java
+++ src/main/java/com/example/controller/TravelController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+// hsyu travel update
 @RestController
 @RequestMapping(value = "/travel")
 public class TravelController {
