015724a39f5ade6a2b524db3d0360babc7dbdbfc
eggmong1007 <eggmong1007@gmail.com>
2021-11-25 17:50:52 +0900

    memberMapper update
    memberMapper update

--- src/main/java/com/example/mappers/MemberMapper.java
+++ src/main/java/com/example/mappers/MemberMapper.java
@@ -11,19 +11,19 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface MemberMapper {
 
-        @Select({ "<script>SELECT * FROM(SELECT ID,EMAIL,GENDER,LOGIN,NAME,REGDATE,ROLE,STATE,ROW_NUMBER() OVER (ORDER BY ${orderbytype} ${orderby}, ID ${orderby}) ROWN FROM MEMBER"
-                        + " <where><if test='state !=null'> AND STATE=#{state}</if>"
-                        + " AND ${keywordtype} LIKE '%' || #{keyword} || '%' </where>) BOARD WHERE ROWN BETWEEN #{start} AND #{end} ORDER BY ROWN ASC"
-                        + " ${orderby} </script>" })
+	@Select({
+			"<script>SELECT * FROM(SELECT ID,EMAIL,GENDER,LOGIN,NAME,REGDATE,ROLE,STATE,ROW_NUMBER() OVER (ORDER BY ${orderbytype} ${orderby}, ID ${orderby}) ROWN FROM MEMBER"
+					+ " <where><if test='state !=null'> AND STATE=#{state}</if>"
+					+ " AND ${keywordtype} LIKE '%' || #{keyword} || '%' </where>) MEMBER WHERE ROWN BETWEEN #{start} AND #{end} ORDER BY ROWN ASC"
+					+ " </script>" })
 
-        public List<MemberDTO> selectMemberAdmin(@Param("keywordtype") String keywordtype,
-                        @Param("keyword") String keyword, @Param("start") int start, @Param("end") int end,
-                        @Param("orderbytype") String orderbytype, @Param("orderby") String orderby,
-                        @Param("state") String state);
+	public List<MemberDTO> selectMemberAdmin(@Param("keywordtype") String keywordtype, @Param("keyword") String keyword,
+			@Param("start") int start, @Param("end") int end, @Param("orderbytype") String orderbytype,
+			@Param("orderby") String orderby, @Param("state") String state);
 
-        @Select({ "<script>SELECT COUNT(*) FROM MEMBER <where><if test='state!=null'> AND STATE=#{state}</if>"
-                        + " AND ${keywordtype} LIKE '%' || #{keyword} || '%' </where></script>" })
-        public int CountMemberAdmin(@Param("keywordtype") String keywordtype, @Param("keyword") String keyword,
-                        @Param("state") String state);
+	@Select({ "<script>SELECT COUNT(*) FROM MEMBER <where><if test='state!=null'> AND STATE=#{state}</if>"
+			+ " AND ${keywordtype} LIKE '%' || #{keyword} || '%' </where></script>" })
+	public int CountMemberAdmin(@Param("keywordtype") String keywordtype, @Param("keyword") String keyword,
+			@Param("state") String state);
 
 }
